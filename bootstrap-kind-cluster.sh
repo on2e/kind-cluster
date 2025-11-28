13071de369f5868d218a31bc1bc86675c0c9bbb7
George Thomadakis <gthomadakis@protonmail.com>
2025-11-29 01:54:24 +0200

    bootstrap-kind-cluster.sh: Quote all variables, rename one
    Signed-off-by: George Thomadakis <gthomadakis@protonmail.com>

--- bootstrap-kind-cluster.sh
+++ bootstrap-kind-cluster.sh
@@ -9,12 +9,12 @@ if ! command -v kind &>/dev/null; then
   exit 1
 fi
 
-REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
-KIND_CLUSTER_NAME=${KIND_CLUSTER_NAME:-"kind"}
-KIND_NODE_IMAGE=${KIND_NODE_IMAGE:-"$(cat KIND_NODE_IMAGE)"}
-KIND_CONFIG=${KIND_CONFIG:-"${REPO_DIR}/config.yaml"}
-KIND_LOG_VERBOSITY=${KIND_LOG_VERBOSITY:-0}
+KIND_CLUSTER_NAME="${KIND_CLUSTER_NAME:-"kind"}"
+KIND_NODE_IMAGE="${KIND_NODE_IMAGE:-"$(cat KIND_NODE_IMAGE)"}"
+KIND_CONFIG="${KIND_CONFIG:-"${REPO_ROOT}/config.yaml"}"
+KIND_LOG_VERBOSITY="${KIND_LOG_VERBOSITY:-"0"}"
 
 if kind get clusters -q | grep -x "${KIND_CLUSTER_NAME}" >/dev/null; then
   echo "kind cluster with name \"${KIND_CLUSTER_NAME}\" already exists"
